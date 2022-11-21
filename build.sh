ab59bd23be7577d57788d2983d27a2e7e73b490e
Ruben Nogueira <ruben@pruvo.com>
2022-11-21 10:32:39 +0000

    feat: build alpine 3.16 image

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-ALPINE_VERSION="3.10"
+ALPINE_VERSION="3.16"
 WKHTMLTOX_VERSION="0.12.6"
 
+docker buildx create --use --platform "linux/arm64,linux/amd64"
 docker buildx build --platform linux/amd64,linux/arm64 . \
 	--build-arg ALPINE_VERSION \
 	--build-arg WKHTMLTOX_VERSION \
 	--push \
-	-t noverant/alpine-wkhtmltopdf-builder:latest \
-	-t noverant/alpine-wkhtmltopdf-builder:$WKHTMLTOX_VERSION-alpine$ALPINE_VERSION \
-	-t noverant/alpine-wkhtmltopdf-builder:$WKHTMLTOX_VERSION-alpine$ALPINE_VERSION-"$GITHUB_RUN_ID"
+	-t rubnogueira/alpine-wkhtmltopdf-builder:latest \
+	-t rubnogueira/alpine-wkhtmltopdf-builder:$WKHTMLTOX_VERSION-alpine$ALPINE_VERSION
