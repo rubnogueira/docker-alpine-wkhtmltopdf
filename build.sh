aafc6442ef9189b76dd07b5f0ac99bdad9e4dde0
Ruben Nogueira <ruben.p.nogueira@gmail.com>
2022-12-07 17:14:45 +0000

    fix: not building arm64 image

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ ALPINE_VERSION="3.16"
 WKHTMLTOX_VERSION="0.12.6"
 
 docker buildx create --use --platform "linux/arm64,linux/amd64"
-docker buildx build --platform linux/amd64,linux/arm64 . \
+docker buildx build --platform "linux/amd64,linux/arm64" . \
 	--build-arg ALPINE_VERSION \
 	--build-arg WKHTMLTOX_VERSION \
 	--push \
